d4cbee518bde1ba4f1e07262a4c6f831dffe64d8
Estmk1995 <ecjarac@puce.edu.ec>
2024-07-07 18:07:52 -0500

    commit de esteban
    inicio de sesion

---
+++ src/pages/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className='principal'>
+      <div className="contenedor-inicio-sesion">
+        <h1>Iniciar Sesión</h1>
+        <form id="formularioInicioSesion">
+          <label htmlFor="usuario">Usuario:</label>
+          <input type="text" id="usuario" aria-label="usuario" required />
+          <label htmlFor="contrasena">Contraseña:</label>
+          <input type="password" id="contrasena" aria-label="contraseña" required />
+          <button type="submit">Entrar</button>
+        </form>
+        <button id="botonRegistro" title="Registrarse para una nueva cuenta">Registrarse</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+
